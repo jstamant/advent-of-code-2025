bf9bb4a7c98cc8b5929d66352c82d9e669ba5eb9
Justin St-Amant <jstamant24@gmail.com>
2025-12-18 04:19:09 -0400

    Setup for day 2

--- aoc/day02.scm
+++ aoc/day02.scm
@@ -2,7 +2,7 @@
   #:use-module (aoc utils)
   #:use-module (ice-9 rdelim)
   #:use-module (ice-9 regex)
-  #:export (part1 part2))
+  #:export (part1 part2 parse-input))
 
 ;; Takes the ranges, ex "11-22", and returns them as cons cells of numbers, ex (11 22)
 (define (parse-input file)
@@ -21,13 +21,13 @@
                      (string-drop string-id (/ length 2))) #t)
           (else #f))))
 
-(define (sum-invalid-ids range)
+(define* (sum-invalid-ids range #:optional (proc invalid-id?))
   (let loop-ids ((ids range)
                  (acc 0))
     (if (> (car ids) (cadr ids))
         acc
         (loop-ids (list (+ 1 (car ids)) (cadr ids))
-                  (+ acc (if (invalid-id? (car ids)) (car ids) 0))))))
+                  (+ acc (if (proc (car ids)) (car ids) 0))))))
 
 ;; answer for part1 example is 1227775554
 ;; answer for part1 is 18952700150
@@ -39,8 +39,17 @@
         (loop-ranges (cdr ranges)
                      (+ acc (sum-invalid-ids (car ranges)))))))
 
-(define (part2 input-filepath)
-  0)
+(define (all-repeating-digits? id)
+  #t)
 
-(part1 (parse-input "input/day02.txt"))
+;; answer for part1 example is 4174379265
+(define (part2 data)
+  (let loop-ranges ((ranges data)
+                    (acc 0))
+    (if (null? ranges)
+        acc
+        (loop-ranges (cdr ranges)
+                     (+ acc (sum-invalid-ids (car ranges) all-repeating-digits?))))))
+
+(part2 (parse-input "input/day02-example.txt"))
 
---
+++ test/day02-test.scm
@@ -0,0 +1,17 @@
+(use-modules (srfi srfi-64)
+             (aoc day02))
+
+(test-begin "day02")
+
+(test-equal "part1 example"
+  1227775554
+  (part1 (parse-input "input/day02-example.txt")))
+(test-equal "part1"
+  18952700150
+  (part1 (parse-input "input/day02.txt")))
+
+(test-equal "part2 example"
+  4174379265
+  (part2 (parse-input "input/day02.txt")))
+
+(test-end)
